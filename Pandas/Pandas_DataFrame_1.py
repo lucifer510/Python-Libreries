63eeff69eb9f7d4808bf957d887dd95ec7d9f2e1
Mihir Prajapati <prajapatimihir799@gmail.com>
2022-08-31 09:14:38 +0530

    Delete Pandas_DataFrame_1.py

--- Pandas/Pandas_DataFrame_1.py
+++
@@ -1,9 +0,0 @@
-# it is 2D Labeled data Structure
-import pandas as pd
-
-df1 = pd.DataFrame({'Name': ['Mihir', 'Raj', 'Yashvi'], 'Marks': [99, 95, 98]})
-print(df1)  # Prints "Name  Marks
-#                0   Mihir     99
-#                1     Raj     95
-#                2  Yashvi     98"
-print(type(df1))  # Prints "<class 'pandas.core.frame.DataFrame'>"
